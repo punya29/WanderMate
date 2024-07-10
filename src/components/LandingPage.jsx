6f42c58c87e095bafcf02608a8458c24e0c4e702
punya29 <salenatamang257@gmail.com>
2024-07-10 12:37:46 +0545

    first commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,21 @@
+import
+
+
+
+
+const LandingPage = () => {
+    return (
+        <>
+            <div 
+                className= "h-screen w-full"
+                style= {{
+                    background: `url(${BGImg})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}
+            >
+            <div className= "
+            </div>
+        </>
+    );
+};
