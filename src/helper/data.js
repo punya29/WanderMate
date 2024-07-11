37e43d65c5b86bfd0160bf15d9821dd16f518df3
punya29 <salenatamang257@gmail.com>
2024-07-11 11:55:10 +0545

    initial commit

---
+++ src/helper/data.js
@@ -0,0 +1,63 @@
+export const landingFooterLinks = [
+    {
+      id: 1,
+      title: "Facebook",
+      link: "https://www.facebook.com",
+    },
+    {
+      id: 2,
+      title: "X",
+      link: "https://www.x.com",
+    },
+    {
+      id: 3,
+      title: "Instagram",
+      link: "https://www.instagram.com",
+    },
+    {
+      id: 4,
+      title: "Youtube",
+      link: "https://www.youtube.com",
+    },
+    {
+      id: 5,
+      title: "Google",
+      link: "https://www.google.com",
+    },
+];
+
+export  const landingHeaderLinks = [{
+    id: 1,
+    title: "About",
+    link: "#"
+  },
+
+{
+    id: 2,
+    title: "Tours",
+    link: "#"
+},
+
+{
+    id: 3,
+    title: "Sale",
+    link: "#"
+},
+
+{
+    id: 4,
+    title: "Contact",
+    link: "#"
+},
+
+{
+    id: 5,
+    title: "Sign In",
+    link: "/signin"
+},
+
+{
+    id: 6,
+    title: "Sign Up",
+    link: "/signup"
+}];
